5497e538e0c862953230dd621d7c6cf7e4bc4c4b
AbelKidaneHaile <abelkidanehaile@gmail.com>
2025-07-11 10:50:52 +0400

    formatted using black

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import streamlit as st
-import serial
 import time
-import src
-from stqdm import stqdm
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import serial
+import streamlit as st
+from stqdm import stqdm
+
+import src
 
 # my variables
 SERIAL_PORT = "COM3"
@@ -13,6 +15,7 @@ MAX_PROGRESS = 24
 
 def dummy_function():
     pass  # placeholder for any future functionality
+
 
 def mysidebar():
     with st.sidebar:
@@ -24,9 +27,11 @@ def mysidebar():
         if time_val < 0:
             st.sidebar.warning("Time must be non-negative.")
             time_val = 0
-        elif time_val > (16-st.session_state.progress):
+        elif time_val > (16 - st.session_state.progress):
             time_val = 1
-            st.sidebar.warning(f"Time must not be above {MAX_PROGRESS}. This could lead to overinflation.")
+            st.sidebar.warning(
+                f"Time must not be above {MAX_PROGRESS}. This could lead to overinflation."
+            )
     except ValueError:
         st.sidebar.warning("Please enter a valid integer.")
         time_val = 0
@@ -34,32 +39,40 @@ def mysidebar():
     # Inflate and Deflate logic
     if st.sidebar.button("Inflate"):
         dummy_function()
-        st.session_state.progress = min(st.session_state.progress + time_val, MAX_PROGRESS)
+        st.session_state.progress = min(
+            st.session_state.progress + time_val, MAX_PROGRESS
+        )
 
     if st.sidebar.button("Deflate"):
         st.session_state.progress = max(st.session_state.progress - time_val, 0)
-    
+
+
 def mymainpage():
     st.title("Data Capture")
-    if 'progress' not in st.session_state:
+    if "progress" not in st.session_state:
         st.session_state.progress = 0
     # Show progress bar
     progress_percent = st.session_state.progress / MAX_PROGRESS
-    st.progress(progress_percent, text=f"Progress: {st.session_state.progress} / {MAX_PROGRESS}")
-    
+    st.progress(
+        progress_percent, text=f"Progress: {st.session_state.progress} / {MAX_PROGRESS}"
+    )
+
     if st.button("Capture Data", type="primary"):
         dummy_function()
-        
-        chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+        chart_data = pd.DataFrame(
+            np.random.randn(20, 1),
+            columns=[
+                "a",
+            ],
+        )
         st.line_chart(chart_data)
-        
+
 
 def main():
     mymainpage()
     mysidebar()
-    
+
 
 if __name__ == "__main__":
     main()
-
-
--- src/rw_json.py
+++ src/rw_json.py
@@ -1,10 +1,12 @@
 import json
 import os
 
+
 def read_json(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
+
 
 def write_json(data, output_path, indent=4):
     with open(output_path, "w", encoding="utf-8") as f:
